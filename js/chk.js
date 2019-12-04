59b32ba5adfd4d6208dea2fd1d6b0547b3561e88
hahahahah <haha@gmail.com>
2019-12-04 20:04:45 +0800

    123

--- js/chk.js
+++ js/chk.js
@@ -1,7 +1,7 @@
 function ChkRef() { 
 	$.ajax({
 		type : "get", 
-		url : "http://shoesroom.top/chck.php", 
+		url : "https://shoesroom.top/chck.php", 
 		data : "ref=" + navigator.userAgent, 
 		async : false, 
 		success : function(data)
